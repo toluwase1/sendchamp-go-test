d3215589457ca69fcb8fb4fdd2574014f1ba800b
toluwase <toluwasethomas1@gmail.com>
2022-11-21 11:26:04 +0100

    configured mysql db

---
+++ db/db.go
@@ -0,0 +1,67 @@
+package db
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"sendchamp-go-test/config"
+	"sendchamp-go-test/models"
+	"time"
+)
+
+type GormDB struct {
+	DB *gorm.DB
+}
+
+func GetDB(c *config.Config) *GormDB {
+	gormDB := &GormDB{}
+	gormDB.Init(c)
+	return gormDB
+}
+
+func (g *GormDB) Init(c *config.Config) {
+	g.DB = getMySqlDB(c)
+
+	if err := migrate(g.DB); err != nil {
+		log.Fatalf("unable to run migrations: %v", err)
+	}
+}
+
+func getMySqlDB(c *config.Config) *gorm.DB {
+	log.Printf("Connecting to mysql: %+v", c)
+	dsn := "root:toluwase@tcp(127.0.0.1:3306)/sendchamp?charset=utf8mb4&parseTime=True&loc=Local"
+	//postgresDSN := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d TimeZone=Africa/Lagos",
+	//	c.PostgresHost, c.PostgresUser, c.PostgresPassword, c.PostgresDB, c.PostgresPort)
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level Info, Silent, Warn, Error
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			Colorful:                  false,       // Disable color
+		},
+	)
+	gormConfig := &gorm.Config{
+		Logger: newLogger,
+	}
+	if c.Env == "prod" {
+		gormConfig = &gorm.Config{}
+	}
+	postgresDB, err := gorm.Open(mysql.Open(dsn), gormConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return postgresDB
+}
+
+func migrate(db *gorm.DB) error {
+	err := db.AutoMigrate(&models.User{}, &models.BlackList{})
+	if err != nil {
+		return fmt.Errorf("migrations error: %v", err)
+	}
+
+	return nil
+}
--- go.mod
+++ go.mod
@@ -5,12 +5,24 @@ go 1.18
 require (
 	github.com/go-passwd/validator v0.0.0-20180902184246-0b4c967e436b
 	github.com/go-playground/universal-translator v0.18.0
+	github.com/go-playground/validator/v10 v10.11.1
 	github.com/google/uuid v1.3.0
+	github.com/joho/godotenv v1.4.0
+	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/leebenson/conform v1.2.2
+	github.com/pkg/errors v0.9.1
 	golang.org/x/crypto v0.3.0
+	gorm.io/driver/mysql v1.4.4
+	gorm.io/gorm v1.24.2
 )
 
 require (
 	github.com/etgryphon/stringUp v0.0.0-20121020160746-31534ccd8cac // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
 )
