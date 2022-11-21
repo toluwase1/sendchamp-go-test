8c1fb49973d437e32566cca8455d85b1cb278854
toluwase <toluwasethomas1@gmail.com>
2022-11-21 17:39:40 +0100

    setup main.go

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"sendchamp-go-test/config"
+	"sendchamp-go-test/db"
+	"sendchamp-go-test/server"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello world")
+	http.DefaultClient.Timeout = time.Second * 10
+	conf, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	gormDB := db.GetDB(conf)
+	authRepo := db.NewAuthRepo(gormDB)
+	authService := services.NewAuthService(authRepo, conf)
+
+	s := &server.Server{
+		Config:         conf,
+		AuthRepository: authRepo,
+		AuthService:    authService,
+	}
+	s.Start()
 }
